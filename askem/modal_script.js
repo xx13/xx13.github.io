0687fd7da18a509afcb51d818230f4912d10e4a8
xx13 <xximsohappy@gmail.com>
2017-02-18 14:46:13 +0300

    Askem
    v1.0

---
+++ askem/modal_script.js
@@ -0,0 +1,8 @@
+function showModal(id) {
+    var x = document.getElementById('modal_' + id);
+    x.style.display = 'block';
+}
+function hideModal(id) {
+    var x = document.getElementById('modal_' + id);
+    x.style.display = 'none';
+}
