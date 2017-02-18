b6808d11b7250215f6c7cf3530cc14cd7b92fc16
xx13 <xximsohappy@gmail.com>
2017-02-18 15:24:42 +0300

    Askem index update
    All blocks pasted manually.

--- askem/modal_script.js
+++
@@ -1,8 +0,0 @@
-function showModal(id) {
-    var x = document.getElementById('modal_' + id);
-    x.style.display = 'block';
-}
-function hideModal(id) {
-    var x = document.getElementById('modal_' + id);
-    x.style.display = 'none';
-}
